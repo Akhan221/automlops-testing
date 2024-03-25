0c707c9b700751726a23becb7cff8cf60c3ea67b
Ahmad Khan <ahmadkh@google.com>
2024-03-25 17:50:44 +0000

    changed directory

--- tests/integration/test_beans_training_model.py
+++ tests/integration/test_beans_training_model.py
@@ -31,7 +31,7 @@ from google_cloud_automlops.utils.constants import (
 def test_beans_training_model():
 
     # Install AutoMLOps from [PyPI](https://pypi.org/project/google-cloud-automlops/), or locally by cloning the repo and running `pip install .`
-    helpers.execute_process('pip3 install .', False)
+    helpers.execute_process('pip3 install ../', False)
 
     # Set your project ID below.
     PROJECT_ID = 'airflow-sandbox-392816'  # @param {type:"string"}
