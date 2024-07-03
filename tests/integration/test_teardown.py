024181cd8135becde9768f9873e8969a1a0e1431
Ahmad Khan <ahmadkh@google.com>
2024-07-03 03:28:16 +0000

    fixed up teardown function for artifact repositories

--- tests/integration/test_teardown.py
+++ tests/integration/test_teardown.py
@@ -21,8 +21,48 @@ def execute_process(command: str, to_null: bool):
         except subprocess.CalledProcessError as err:
             raise RuntimeError(f'Error executing process. {err}') from err
 
-def teardown_gcloud_resources(project_id, artifact_repo_location): 
-    execute_process(f"gcloud artifacts repositories list --project={project_id} --location={artifact_repo_location}", False)
+def string_execute_process(command: str, to_null: bool = False) -> str:
+    """Executes an external shell process and captures its output.
+
+    Args:
+        command: The string of the command to execute.
+        to_null: If True, output will be sent to /dev/null; otherwise, it's captured.
+
+    Returns:
+        The combined standard output (stdout) and standard error (stderr) as a string.
+
+    Raises:
+        RuntimeError: If an error occurs during command execution.
+    """
+
+    try:
+        result = subprocess.run(
+            command,
+            shell=True,  # Interpret the command as a shell command
+            check=True,   # Raise an exception if the command returns a non-zero exit code
+            stdout=subprocess.PIPE,  # Capture stdout
+            stderr=subprocess.STDOUT,  # Combine stderr with stdout
+            text=True,     # Decode output as text (Python 3.7+)
+        )
+
+        if to_null:
+            return ""  # Return an empty string if output is directed to /dev/null
+        else:
+            return result.stdout  # Return the captured output as a string
+
+    except subprocess.CalledProcessError as err:
+        raise RuntimeError(f'Error executing process: {err}') from err
+
+
+
+def teardown_gcloud_resources(project_id, artifact_repo_location, artifact_repo_name="dry-beans-dt-inferencing-artifact-registry"): 
+    artifact_list = string_execute_process(f'gcloud artifacts repositories list --project={project_id} --location={artifact_repo_location}', False)
+    if artifact_repo_name in artifact_list:
+        print("This is the artifact list: ", artifact_list)
+        outputs = string_execute_process(f"gcloud artifacts repositories delete {artifact_repo_name} --location={artifact_repo_location} --quiet")
+        print(outputs)
+    else:
+        print(f"The artifact registry {artifact_repo_name} doesn't exist in {artifact_repo_location}")
 
 teardown_gcloud_resources("airflow-sandbox-392816", "us-central1")
 
